c497c85bf52d25a2224e01a60049938a35823b9c
Gora LEYE <tocane.technologies@gmail.com>
2019-09-29 13:30:04 +0200

    GetPlayers Should Return Ok

--- LogCorner.Hackaton.TennisPlayer.Presentation.Specs/TennisPlayerControllerSpecs.cs
+++ LogCorner.Hackaton.TennisPlayer.Presentation.Specs/TennisPlayerControllerSpecs.cs
@@ -1,8 +1,10 @@
-﻿using LogCorner.Hackaton.TennisPlayer.Application;
+﻿using System.Collections.Generic;
+using LogCorner.Hackaton.TennisPlayer.Application;
 using LogCorner.Hackaton.TennisPlayer.Presentation.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using System.Threading.Tasks;
+using LogCorner.Hackaton.TennisPlayer.Domain;
 using Xunit;
 
 namespace LogCorner.Hackaton.TennisPlayer.Presentation.Specs
@@ -14,10 +16,31 @@ namespace LogCorner.Hackaton.TennisPlayer.Presentation.Specs
         {
             //Arrange
             Mock<IGetPlayersUsesCase> moqGetPlayersUsesCase = new Mock<IGetPlayersUsesCase>();
-            var sut = new TennisPlayerController(moqGetPlayersUsesCase.Object);
+
+            IEnumerable<Player> players = new List<Player>();
+            moqGetPlayersUsesCase.Setup(m => m.Handle()).Returns(Task.FromResult(players));
+            var sut = new TennisPlayerController(moqGetPlayersUsesCase.Object,It.IsAny<IGetPlayerUsesCase>());
 
             //Act
             var result = await sut.Get();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+
+        [Fact(DisplayName = "GetPlayers Should Return Ok")]
+        public async Task GetPlayerShouldReturnOk()
+        {
+            //Arrange
+            Mock<IGetPlayerUsesCase> moqGetPlayerUsesCase = new Mock<IGetPlayerUsesCase>();
+            var player = new Player(2, "name2", "surname2", "M", It.IsAny<Country>(), "", It.IsAny<Data>());
+            moqGetPlayerUsesCase.Setup(m => m.Handle(It.IsAny<PlayerRequest>())).Returns(Task.FromResult(player));
+            var sut = new TennisPlayerController(It.IsAny<IGetPlayersUsesCase>(), moqGetPlayerUsesCase.Object);
+
+            //Act
+            var result = await sut.Get(player.Id);
 
             //Assert
             Assert.NotNull(result);
--- LogCorner.Hackaton.TennisPlayer.Presentation/Controllers/TennisPlayerController.cs
+++ LogCorner.Hackaton.TennisPlayer.Presentation/Controllers/TennisPlayerController.cs
@@ -9,16 +9,25 @@ namespace LogCorner.Hackaton.TennisPlayer.Presentation.Controllers
     public class TennisPlayerController : ControllerBase
     {
         private readonly IGetPlayersUsesCase _listPlayersUsesCase;
+        private readonly IGetPlayerUsesCase _getPlayersUsesCase;
 
-        public TennisPlayerController(IGetPlayersUsesCase listPlayersUsesCase)
+        public TennisPlayerController(IGetPlayersUsesCase listPlayersUsesCase, IGetPlayerUsesCase getPlayersUsesCase)
         {
             _listPlayersUsesCase = listPlayersUsesCase;
+            _getPlayersUsesCase = getPlayersUsesCase;
         }
 
         [HttpGet]
         public async Task<IActionResult> Get()
         {
             var result = await _listPlayersUsesCase.Handle();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _getPlayersUsesCase.Handle(new PlayerRequest(id));
             return Ok(result);
         }
     }
