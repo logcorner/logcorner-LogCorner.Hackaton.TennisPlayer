df5e498edde64abe03d8dc3677be1d55b45da2f4
Gora LEYE <tocane.technologies@gmail.com>
2019-09-29 13:21:56 +0200

    GetPlayers Should Return Ok

---
+++ LogCorner.Hackaton.TennisPlayer.Presentation.Specs/TennisPlayerControllerSpecs.cs
@@ -0,0 +1,27 @@
+﻿using LogCorner.Hackaton.TennisPlayer.Application;
+using LogCorner.Hackaton.TennisPlayer.Presentation.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LogCorner.Hackaton.TennisPlayer.Presentation.Specs
+{
+    public class TennisPlayerControllerSpecs
+    {
+        [Fact(DisplayName = "GetPlayers Should Return Ok")]
+        public async Task GetPlayersShouldReturnOk()
+        {
+            //Arrange
+            Mock<IGetPlayersUsesCase> moqGetPlayersUsesCase = new Mock<IGetPlayersUsesCase>();
+            var sut = new TennisPlayerController(moqGetPlayersUsesCase.Object);
+
+            //Act
+            var result = await sut.Get();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<OkObjectResult>(result);
+        }
+    }
+}
---
+++ LogCorner.Hackaton.TennisPlayer.Presentation/Controllers/TennisPlayerController.cs
@@ -0,0 +1,25 @@
+﻿using LogCorner.Hackaton.TennisPlayer.Application;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LogCorner.Hackaton.TennisPlayer.Presentation.Controllers
+{
+    [Route("api/tennis-player")]
+    [ApiController]
+    public class TennisPlayerController : ControllerBase
+    {
+        private readonly IGetPlayersUsesCase _listPlayersUsesCase;
+
+        public TennisPlayerController(IGetPlayersUsesCase listPlayersUsesCase)
+        {
+            _listPlayersUsesCase = listPlayersUsesCase;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _listPlayersUsesCase.Handle();
+            return Ok(result);
+        }
+    }
+}
